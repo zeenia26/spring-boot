f226211099c41ed4aa808a620d1d0927a88046ba
zeenia26 <82228863+zeenia26@users.noreply.github.com>
2023-11-16 09:40:15 +0530

    Update db_backup.sql

--- Docker-files/db/db_backup.sql
+++ Docker-files/db/db_backup.sql
@@ -1,4 +1,4 @@
-USE bugtracker;
+USE springdb;
 
 CREATE TABLE `status`
 (
@@ -46,27 +46,27 @@ CREATE TABLE `item`
     FOREIGN KEY (`member_id`) REFERENCES team_members(`member_id`)
 );
 
-INSERT INTO bugtracker.team_members VALUES (1, 'John', 'Thompson', 'j.thompson@spring.com');
-INSERT INTO bugtracker.team_members VALUES (2, 'Mary', 'Johnson', 'm.johnson@spring.com');
-INSERT INTO bugtracker.team_members VALUES (3, 'Judy', 'Gordon', 'j.gordon@spring.com');
-INSERT INTO bugtracker.team_members VALUES (4, 'Frank', 'Williams', 'f.williams@spring.com');
+INSERT INTO springdb.team_members VALUES (1, 'John', 'Thompson', 'j.thompson@spring.com');
+INSERT INTO springdb.team_members VALUES (2, 'Mary', 'Johnson', 'm.johnson@spring.com');
+INSERT INTO springdb.team_members VALUES (3, 'Judy', 'Gordon', 'j.gordon@spring.com');
+INSERT INTO springdb.team_members VALUES (4, 'Frank', 'Williams', 'f.williams@spring.com');
 
-INSERT INTO bugtracker.status VALUES (1,'To Do');
-INSERT INTO bugtracker.status VALUES (2,'In Progress');
-INSERT INTO bugtracker.status VALUES (3,'To Validate');
-INSERT INTO bugtracker.status VALUES (4,'Done');
+INSERT INTO springdb.status VALUES (1,'To Do');
+INSERT INTO springdb.status VALUES (2,'In Progress');
+INSERT INTO springdb.status VALUES (3,'To Validate');
+INSERT INTO springdb.status VALUES (4,'Done');
 
-INSERT INTO bugtracker.type VALUES (1,'Task');
-INSERT INTO bugtracker.type VALUES (2,'Study');
-INSERT INTO bugtracker.type VALUES (3,'Support');
-INSERT INTO bugtracker.type VALUES (4,'Bug');
+INSERT INTO springdb.type VALUES (1,'Task');
+INSERT INTO springdb.type VALUES (2,'Study');
+INSERT INTO springdb.type VALUES (3,'Support');
+INSERT INTO springdb.type VALUES (4,'Bug');
 
-INSERT INTO bugtracker.project VALUES (1,'Project Metro','Increase productivity',1, 1);
-INSERT INTO bugtracker.project VALUES (2,'Project Ivory','Reduce project time and cost',3, 2);
-INSERT INTO bugtracker.project VALUES (3,'Project Apollo','Improve communication between teams',4, 3);
-INSERT INTO bugtracker.project VALUES (4,'Project Kodiak','Identify innovative solutions and approaches',3, 4);
+INSERT INTO springdb.project VALUES (1,'Project Metro','Increase productivity',1, 1);
+INSERT INTO springdb.project VALUES (2,'Project Ivory','Reduce project time and cost',3, 2);
+INSERT INTO springdb.project VALUES (3,'Project Apollo','Improve communication between teams',4, 3);
+INSERT INTO springdb.project VALUES (4,'Project Kodiak','Identify innovative solutions and approaches',3, 4);
 
-INSERT INTO bugtracker.item VALUES (1, 'Implement Project Apollo', 'Implement the results of Project Apollo', 1, 1, 1);
-INSERT INTO bugtracker.item VALUES (2, 'Project Apollo: PIR', 'Conduct a PIR of Project Apollo', 2, 2, 2);
-INSERT INTO bugtracker.item VALUES (3, 'Define tasks for Project Metro', 'Define project plan and Roadmap', 4, 2, 3);
-INSERT INTO bugtracker.item VALUES (4, 'Fix website bug', 'Test website using different browsers', 3, 4, 4);
+INSERT INTO springdb.item VALUES (1, 'Implement Project Apollo', 'Implement the results of Project Apollo', 1, 1, 1);
+INSERT INTO springdb.item VALUES (2, 'Project Apollo: PIR', 'Conduct a PIR of Project Apollo', 2, 2, 2);
+INSERT INTO springdb.item VALUES (3, 'Define tasks for Project Metro', 'Define project plan and Roadmap', 4, 2, 3);
+INSERT INTO springdb.item VALUES (4, 'Fix website bug', 'Test website using different browsers', 3, 4, 4);
